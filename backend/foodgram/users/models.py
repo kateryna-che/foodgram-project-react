2f08c28adcfba5a9a58806b7d02764e181b3c2ee
Kateryna Cherepanova <absent.kai@gmail.com>
2023-06-14 21:30:36 +0300

    Созданы модели в приложении Users

---
+++ backend/foodgram/users/models.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        'Электронная почта',
+        max_length=254,
+        unique=True
+    )
+    username = models.CharField(
+        'Логин',
+        max_length=150,
+        unique=True
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=150
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150
+    )
+    password = models.CharField(
+        'Пароль',
+        max_length=150
+    )
+
+    def __str__(self):
+        return str(self.username)
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Пользователь'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор'
+    )
