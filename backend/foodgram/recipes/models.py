8ed2ab0afff520abe7e8fc251fc157fdc17c6aa6
Kateryna Cherepanova <absent.kai@gmail.com>
2023-06-14 23:38:01 +0300

    Созданы модель рецепта

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from ..users.models import User
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор'
+    )
+    name = models.CharField(
+        'Название',
+        max_length=200
+    )
+    image = models.ImageField(
+    )
+    text = models.TextField(
+        'Текстовое описание',
+    )
+    ingredients = models.ManyToManyField()
+    tags = models.ManyToManyField()
+    cooking_time = models.PositiveIntegerField(
+        'Время приготовления(в минутах)',
+    )
