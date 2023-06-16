9a719714598527ad4e765f0d6f0f4b19d5acebaf
Kateryna Cherepanova <absent.kai@gmail.com>
2023-06-16 19:48:20 +0300

    Добавлена модель Тег

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -3,6 +3,25 @@ from django.db import models
 from users.models import User
 
 
+class Tag(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=200
+    )
+    color = models.CharField(
+        'Цвет',
+        max_length=7
+    )
+    slug = models.SlugField(
+        'Slug',
+        max_length=200,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
 class Recipe(models.Model):
     author = models.ForeignKey(
         User,
