0787794d9e2c5a450e42ef09da8ec76c7ca5b685
Kateryna Cherepanova <absent.kai@gmail.com>
2023-06-16 19:48:03 +0300

    Изменено название модели

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -4,7 +4,7 @@ from .models import User
 
 
 @admin.register(User)
-class RecipesUser(admin.ModelAdmin):
+class UserAdmin(admin.ModelAdmin):
     list_display = 'username'
     list_filter = ('email', 'username',)
     empty_value_display = '-пусто-'
