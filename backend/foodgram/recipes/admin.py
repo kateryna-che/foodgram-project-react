eb297b2f73cd33bdf79fc2da920c4bad67b34735
Kateryna Cherepanova <absent.kai@gmail.com>
2023-06-17 21:29:55 +0300

    Зарегистрированны модели в админке

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
-from .models import Recipe, Tag
+from .models import Recipe, Tag, Ingredient, IngredientRecipe, TagRecipe, Favorite, ShoppingCart
+
+
+class IngredientRecipeInline(admin.TabularInline):
+    model = IngredientRecipe
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'author', 'name')
-    list_filter = ('author', 'name')
+    list_display = ('name', 'author', 'in_favorite')
+    list_filter = ('author', 'name', 'tags')
     empty_value_display = '-пусто-'
+    inlines = [IngredientRecipeInline, ]
+
+    def in_favorite(self, obj):
+        return obj.favorite.count()
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    list_filter = ('name',)
 
 
 admin.site.register(Tag)
+admin.site.register(IngredientRecipe)
+admin.site.register(TagRecipe)
+admin.site.register(Favorite)
+admin.site.register(ShoppingCart)
