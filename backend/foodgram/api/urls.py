49d7f1d3afd4a3c9b3d0b5b346357ca693592aa7
Kateryna Cherepanova <absent.kai@gmail.com>
2023-06-21 02:18:20 +0300

    Настроены урлы

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,10 +1,20 @@
 from django.urls import include, path
 from rest_framework import routers
 
+from .views import (IngredientViewSet, RecipeViewSet, SubscribeView,
+                    SubscriptionsListView, TagViewSet)
+
 app_name = 'api'
 
 router = routers.DefaultRouter()
+router.register(r'recipes', RecipeViewSet, basename='recipes')
+router.register(r'tags', TagViewSet, basename='tags')
+router.register(r'ingredients', IngredientViewSet, basename='ingredient')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('users/subscriptions/', SubscriptionsListView.as_view(), name='subscriptions'),
+    path('users/<int:id>/subscribe/', SubscribeView.as_view(), name='subscribe'),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
