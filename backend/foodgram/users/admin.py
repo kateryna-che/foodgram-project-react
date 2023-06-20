37d79b3e214c8b8c18d4ff6349a05dfb1da0ef07
Kateryna Cherepanova <absent.kai@gmail.com>
2023-06-21 02:16:17 +0300

    isort

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User
+from .models import Subscription, User
 
 
 @admin.register(User)
@@ -8,3 +8,6 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('username',)
     list_filter = ('email', 'username',)
     empty_value_display = '-пусто-'
+
+
+admin.site.register(Subscription)
