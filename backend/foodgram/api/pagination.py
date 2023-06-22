8d453056e2ca94d5a919d64e4312319649d7cf91
Kateryna Cherepanova <absent.kai@gmail.com>
2023-06-22 07:20:21 +0300

    Удален ненужный пагинатор

--- backend/foodgram/api/pagination.py
+++ backend/foodgram/api/pagination.py
@@ -1,10 +1,6 @@
 from rest_framework.pagination import PageNumberPagination
 
 
-class RecipesPagination(PageNumberPagination):
-    page_size = 6
-
-
 class CustomPagination(PageNumberPagination):
     page_size_query_param = 'limit'
     max_page_size = 100
