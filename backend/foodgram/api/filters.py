cf1c6462cc185e1b8ba697ad4c6dd4281544dc4e
Kateryna Cherepanova <absent.kai@gmail.com>
2023-06-22 07:14:03 +0300

    Доработаны фильтры

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,23 +1,45 @@
 from django_filters import rest_framework as filters
 
-from recipes.models import Ingredient, Recipe
+from recipes.models import Ingredient, Recipe, Tag
 
 
 class RecipeFilter(filters.FilterSet):
-    tags = filters.CharFilter(field_name='tags__name', method='filter_tags')
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
+        conjoined=False,
+    )
 
-    def filter_tags(self, queryset, name, value):
-        tag_names = value.split(',')
-        return queryset.filter(tags__name__in=tag_names)
+    is_favorited = filters.BooleanFilter(
+        method='get_is_favorited'
+    )
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='get_is_in_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
-        fields = ['tags']
+        fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
+
+    def get_is_favorited(self, queryset, name, value):
+        if self.request.user.is_authenticated and value:
+            return queryset.filter(favorites__user=self.request.user)
+        return queryset
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        if self.request.user.is_authenticated and value:
+            return queryset.filter(shopping_cart__user=self.request.user)
+        return queryset
+
+    class Meta:
+        model = Recipe
+        fields = ('author', 'tags',)
 
 
 class IngredientFilter(filters.FilterSet):
-    name = filters.CharFilter(field_name='name', lookup_expr='istartswith')
+    name = filters.CharFilter(lookup_expr='istartswith')
 
     class Meta:
         model = Ingredient
-        fields = ['name']
+        fields = ('name', )
