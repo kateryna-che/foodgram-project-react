0492f3a89d2320c4b9e701ff043786c8424cfc6a
Kateryna Cherepanova <absent.kai@gmail.com>
2023-06-24 05:50:41 +0300

    Добавлено использование констант, строковое представление и verbose_name

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .constants import MAX_LENGTH
+
 
 class User(AbstractUser):
     email = models.EmailField(
@@ -10,24 +12,32 @@ class User(AbstractUser):
     )
     username = models.CharField(
         'Логин',
-        max_length=150,
+        max_length=MAX_LENGTH,
         unique=True
     )
     first_name = models.CharField(
         'Имя',
-        max_length=150
+        max_length=MAX_LENGTH
     )
     last_name = models.CharField(
         'Фамилия',
-        max_length=150
+        max_length=MAX_LENGTH
     )
     password = models.CharField(
         'Пароль',
-        max_length=150
+        max_length=MAX_LENGTH
     )
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        ordering = ['-username']
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
     def __str__(self):
-        return str(self.username)
+        return self.username
 
 
 class Subscription(models.Model):
@@ -45,7 +55,17 @@ class Subscription(models.Model):
     )
 
     class Meta:
-        constraints = (models.UniqueConstraint(
+        constraints = [models.UniqueConstraint(
             fields=['user', 'author'],
             name='unique_user_author',
-        ),)
+        ),
+            models.CheckConstraint(
+                name="%(app_label)s_%(class)s_prevent_self_follow",
+                check=~models.Q(user=models.F("author")),
+            )
+        ]
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self):
+        return f'{self.user} подписан на {self.author}'
