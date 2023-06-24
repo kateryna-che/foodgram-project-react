e93ea4317a044ab75ba84e396d7e442747f0ffa1
Kateryna Cherepanova <absent.kai@gmail.com>
2023-06-24 05:51:05 +0300

    max_page_size изменено на page_size

--- backend/foodgram/api/pagination.py
+++ backend/foodgram/api/pagination.py
@@ -3,4 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 
 class CustomPagination(PageNumberPagination):
     page_size_query_param = 'limit'
-    max_page_size = 100
+    page_size = 6
